b9219b44e226edf19b97d25cd43fc0fe484f4271
charolite-technocode <104000574+charolite-technocode@users.noreply.github.com>
2022-07-11 14:27:42 +0000

    commit

---
+++ api/oda/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('devant/', views.devant, name='devant'),
+    path('arriere/', views.arriere, name='arriere'),
+    path('gauche/', views.gauche, name='gauche'),
+    path('droite/', views.droite, name='droite'),
+    path('stop/', views.stop, name='stop'),
+]
---
+++ api/oda/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(['GET'])
+def devant(request):
+    return Response({'message': 'OK'})
+
+@api_view(['GET'])
+def arriere(request):
+    return Response({'message': 'OK'})
+
+@api_view(['GET'])
+def gauche(request):
+    return Response({'message': 'OK'})
+
+@api_view(['GET'])
+def droite(request):
+    return Response({'message': 'OK'})
+
+@api_view(['GET'])
+def stop(request):
+    return Response({'message': 'OK'})
